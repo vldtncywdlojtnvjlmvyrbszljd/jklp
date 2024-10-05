a3335e5fa827e12a0f21c937d9eceb92424f919f
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-10-06 00:26:46 +0700

    Update and rename 3.lua to 4.lua

--- 4.lua
+++ 4.lua
@@ -14,32 +14,6 @@ local function SendWebhook(webhookUrl, data)
     end
 end
 
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function AntiBan()
-    for i,v in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
-        if v:IsA("LocalScript") then
-            if v.Name == "General" or v.Name == "Shiftlock"  or v.Name == "FallDamage" or v.Name == "4444" or v.Name == "CamBob" or v.Name == "JumpCD" or v.Name == "Looking" or v.Name == "Run" then
-                v:Destroy()
-            end
-        end
-     end
-     for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerScripts:GetDescendants()) do
-        if v:IsA("LocalScript") then
-            if v.Name == "RobloxMotor6DBugFix" or v.Name == "Clans"  or v.Name == "Codes" or v.Name == "CustomForceField" or v.Name == "MenuBloodSp"  or v.Name == "PlayerList" then
-                v:Destroy()
-            end
-        end
-     end
-    end
-    AntiBan()
-
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-	game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-	wait(1)
-	game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 
 local location
 if game.PlaceId == 2753915549 then
@@ -109,6 +83,55 @@ elseif game:GetService("ReplicatedStorage"):FindFirstChild("Diablo") or game:Get
     SendEliteWebhook("Diablo")
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local LocalPlayer = Players.LocalPlayer
+
+local function AutoJoinTeam()
+
+    local teams = {"Marines"}
+    local chosenTeam = teams[math.random(1, #teams)]
+
+    local ChooseTeamRemote = ReplicatedStorage:FindFirstChild("Remotes"):FindFirstChild("ChooseTeam")
+    if ChooseTeamRemote then
+        ChooseTeamRemote:InvokeServer(chosenTeam)
+        print("Bergabung dengan tim: " .. chosenTeam)
+    else
+        warn("Tidak dapat menemukan RemoteFunction untuk memilih tim.")
+    end
+end
+
+-- Jalankan fungsi setelah pemain bergabung ke dalam game
+LocalPlayer.CharacterAdded:Connect(function()
+    AutoJoinTeam()
+end)
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function AntiBan()
+    for i,v in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
+        if v:IsA("LocalScript") then
+            if v.Name == "General" or v.Name == "Shiftlock"  or v.Name == "FallDamage" or v.Name == "4444" or v.Name == "CamBob" or v.Name == "JumpCD" or v.Name == "Looking" or v.Name == "Run" then
+                v:Destroy()
+            end
+        end
+     end
+     for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerScripts:GetDescendants()) do
+        if v:IsA("LocalScript") then
+            if v.Name == "RobloxMotor6DBugFix" or v.Name == "Clans"  or v.Name == "Codes" or v.Name == "CustomForceField" or v.Name == "MenuBloodSp"  or v.Name == "PlayerList" or v.Name == "FastAttack" or v.Name == "BringMobs" or v.Name == "SpamSkill" or v.Name == "ReplicatedStorage.Effect" then
+                v:Destroy()
+            end
+        end
+     end
+    end
+    AntiBan()
+
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+	game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+	wait(1)
+	game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 local Notif = {}
 
