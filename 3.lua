14039954361ac5cfed97e7f2659a76125d642aca
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-10-07 17:23:11 +0700

    Update and rename 2.lua to 3.lua

--- 3.lua
+++ 3.lua
@@ -7,7 +7,7 @@ screenGui.Parent = LocalPlayer:WaitForChild("PlayerGui")
 
 local frame = Instance.new("Frame")
 frame.Size = UDim2.new(0, 300, 0, 250)
-frame.Position = UDim2.new(0.2, -150, 0.5, -100)
+frame.Position = UDim2.new(0.5, -150, 0.5, -125)
 frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 frame.BorderSizePixel = 2
 frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
@@ -95,7 +95,7 @@ getKeyButton.Text = "Get Key"
 getKeyButton.Font = Enum.Font.SourceSansBold
 getKeyButton.TextSize = 18
 getKeyButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-getKeyButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) --0, 170, 0
+getKeyButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) 
 getKeyButton.Parent = frame
 
 local checkKeyButton = Instance.new("TextButton")
@@ -105,7 +105,7 @@ checkKeyButton.Text = "Check Key"
 checkKeyButton.Font = Enum.Font.SourceSansBold
 checkKeyButton.TextSize = 18
 checkKeyButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-checkKeyButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) --0, 170, 0
+checkKeyButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) 
 checkKeyButton.Parent = frame
 
 local DiscordButton = Instance.new("TextButton")
