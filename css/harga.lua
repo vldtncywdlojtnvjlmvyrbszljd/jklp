474d209a936e72135a57b61853ade05bb996b30a
05650198416531d21d9 <160678881+vldtncywdlojtnvjlmvyrbszljd@users.noreply.github.com>
2024-10-08 23:49:35 +0700

    Update and rename price.lua to harga.lua

--- css/harga.lua
+++ css/harga.lua
@@ -1,9 +1,9 @@
 getgenv().Games = {
-    [2753915549] = "https://37uzdt26sof4b.ahost.marscode.site/mekmek/bf.lua", -- Bl 1
-    [4442272183] = "https://37uzdt26sof4b.ahost.marscode.site/mekmek/bf.lua", -- Bl 2
-    [7449423635] = "https://37uzdt26sof4b.ahost.marscode.site/mekmek/bf.lua", -- Bl 3
-    [10260193230] = "https://37uzdt26sof4b.ahost.marscode.site/mekmek/meme.lua", -- Memesea
-    [13772394625] = "https://37uzdt26sof4b.ahost.marscode.site/mekmek/bladebll.lua", -- Blade Ball
+    [2753915549] = "https://37uzdt26sof4b.ahost.marscode.site/bf.lua", -- Bl 1
+    [4442272183] = "https://37uzdt26sof4b.ahost.marscode.site/bf.lua", -- Bl 2
+    [7449423635] = "https://37uzdt26sof4b.ahost.marscode.site/bf.lua", -- Bl 3
+    [10260193230] = "https://37uzdt26sof4b.ahost.marscode.site/meme.lua", -- Memesea
+    [13772394625] = "https://37uzdt26sof4b.ahost.marscode.site/bladebll.lua", -- Blade Ball
     [15744137588] = "", --Blade Slayer
     [11520107397] = "https://37uzdt26sof4b.ahost.marscode.site/mekmek/slap-battle.lua", --Slap Battle 1
     [6403373529] = "hhttps://37uzdt26sof4b.ahost.marscode.site/mekmek/slap-battle.lua", --Slap Battle 2
@@ -13,3 +13,13 @@ getgenv().Games = {
     [17503543197] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 3
     [10449761463] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/The%20Strongest%20Battleground.lua",  -- The Strongest Battlegrounds
   }
+
+-- Fungsi untuk mendeteksi PlaceId dan menjalankan script yang sesuai
+local placeId = game.PlaceId  -- Mengambil PlaceId dari game yang sedang dijalankan
+
+if getgenv().Games[placeId] then
+    local scriptUrl = getgenv().Games[placeId]
+    loadstring(game:HttpGet(scriptUrl))()  -- Memuat dan menjalankan script dari URL
+else
+    print("Script untuk game ini tidak tersedia.")
+end
